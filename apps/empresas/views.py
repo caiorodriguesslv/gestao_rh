d6800462caeea6c554338d135efb36607abaec0d
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-07-18 04:41:19 -0300

    Vinculando funcionario logado

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 from .models import Empresa
 
 
 class EmpresaCreate(CreateView):
     model = Empresa
-    fields = {'nome'}
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Empresa Cadastrada')
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
