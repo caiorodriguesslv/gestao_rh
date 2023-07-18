bb4a145d4acfd741b4c5ff3ac9eea0ffb33ad660
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-07-17 22:53:43 -0300

    Crud App empresas

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
+    path('empresas/', include('apps.empresas.urls')),
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
